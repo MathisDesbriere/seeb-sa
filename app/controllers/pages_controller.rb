77fb88ce4331e95df10f86fcbc3b223d1500e419
Mathis Desbriere <mathis.desbriere@hotmail.fr>
2023-08-08 16:35:02 +0200

    heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
+    # The `geocoded` scope filters only flats with coordinates
+    @markers =
+      [{
+        lat: 48.91729908603644,
+        lng: 2.5017622964173816
+      }]
   end
 end
